256cb8ef20ab1f431c3c32659e46fa0693c7a7c2
nirmala <nirmalasilalahi8@gmail.com>
2023-11-19 19:40:11 +0700

    PW4_UTS_2214-1118

---
+++ script.js
@@ -0,0 +1,115 @@
+function showCardInfo(cardNumber) {
+    // Simulasikan pengambilan informasi dari server atau sumber data lainnya
+    var cardInfo;
+    if (cardNumber === 1) {
+        cardInfo = "Lagu di album debut Taylor Swift: <br> 1. Tim Mc Graw\n2. Teardrops on My Guitar\n3. Our Song\n4. Picture to Burn\n5. Should've Said No";
+    } else if (cardNumber === 2) {
+        cardInfo = "Lagu di album Fearless: <br> 1. Fearless (Taylor's Version)\n2. Fifteen (Taylor's Version\n3. Love Story (Taylor's Version)\n4. Hey Stephen (Taylor's Version)\n5. White Horse (Taylor's Version)\n6. You Belong With Me (Taylor's Version)\n7. Breathe\n 8. Tell Me Why (Taylor's Version)\n9. You're Not Sorry (Taylor's Version)\n10. The Way I Loved You (Taylor's Version)\n11. Forever & Always (Taylor's Version)\n12. The Best Day (Taylor's Version)\n13. Change (Taylor's Version)\n14. Jump Then Fall (Taylor's Version)\n15. Untouchable (Taylor's Version)\n16. Come In With The Rain (Taylor's Version)\n17. Superstar (Taylor's Version)\n19. The Other Side of The Door (Taylor's Version)\n20. Today Was A Fairytale (Taylor's Version)\n21. You All Over Me\n22. Mr. Perfectly Fine (Taylor's Version)\n23. We Were Happy (Taylor's Version)\n24. That's When\n25. Don't You (Taylor's Version)\n25. Bye Bye Baby (Taylor's Version)";
+    }  if (cardNumber === 3) {
+        cardInfo = "Lagu di album Speak Now: <br> 1. Mine (Taylor's Version)\n2. Sparks Fly (Taylor's Version)\n3. Back To December (Taylor's Version)\n4. Speak Now (Taylor's Version)\n5. Dear John (Taylor's Version)\n6. Mean (Taylor's Version)\n7. The Story of Us (Taylor's Version)\n8. Never Grow Up (Taylor's Version)\n 9. Enchanted (Taylor's Version)\n 10. Better Than Revenge (Taylor's Version)\n11. Innocent (Taylor's Version)\n12. Haunted (Taylor's Version)\n13. Last Kiss (Taylor's Version)\n14. Long Live (Taylor's Version)\n15. Ours (Taylor's Version)\n16. Superman (Taylor's Version)\n17. Electric Touch (feat. Fall Out Boy)(Taylor's Version)\n18. When Emma Falls in Love (Taylor's Version)\n19. I Can See You (Taylor's Version)\n20. Castles Crumbling (feat.Hayley Williams)(Taylor's Version)\n21. Foolish One (Taylor's Version)\n22. Timeless (Taylor's Version)";
+    } else if (cardNumber === 4) {
+        cardInfo = "Lagu di album Lover: <br> 1. State of Grace (Taylor's Version)\n2. Red (Taylor's Version)\n3. Treacherous (Taylor's Version)\n4. I Knew You Were Trouble (Taylor's Version)\n5. All Too Well (Taylor's Version)\n6. 22 (Taylor's Version)\n7. I Almost Do (Taylor's Version)\n8. We are Never Ever Getting Back Together (Taylor's Version)\n9. Stay Stay Stay (Taylor's Version)\n10. The Last Time\n11. Sad Beautiful Tragic (Taylor's Version)\n12. The Lucky One (Taylor's Version)\n13. Everything Has Changed (Taylor's Version)\n14. Starlight (Taylor's Version)\n15. Begin Again (Taylor's Version)\n16. The Moment I Knew (Taylor's Version)\n17. Come Back... Be Here (Taylor's Version)\n18. Girl at Home (Taylor's Version)\n19. State of Grace (Accoustic Version)\n20. Ronan (Taylor's Version)\n21. Better Man (Taylor's Version)\n22. Nothing New\n23. Babe (Taylor's Version)\n24. Message in a Bottle (Taylor's Version)\n25. I Bet You Think About Me (Taylor's Version)\n26. Forever Winter (Taylor's Version)\n27. Run\n28. The Very First Night (Taylor's Version)\n29. All Too Well (10 Minutes Version)(Taylor's Version)";
+    } else if (cardNumber === 5) {
+        cardInfo = "Lagu di album 1989: <br> 1. Welcome to New York (Taylor's Version)\n2. Blank Space (Taylor's Version)\n3. Style (Taylor's Version)\n4. Out of The Woods (Taylor's Version)\n5. All You Had TO Do Was Stay (Taylor's Version)\n6. Shake it Off (Taylor's Version)\n7. I Wish You Would (Taylor's Version)\n8. Bad Blood (Taylor's Version)\n9. Wildest Dreams (Taylor's Version)\n10. How You Get The Girl (Taylor's Version)\n11. This Love (Taylor's Version)\n12. I Know Place (Taylor's Version)\n13. Clean (Taylor's Version)\n14. Wonderland (Taylor's Version)\n15. You Are In Love (Taylor's Version)\n16. New Romantics (Taylor's Version)\n17. 'Slut!' (Taylor's Version)\n18. Say Don't Go (Taylor's Version)\n19. Now That We Don't Talk (Taylor's Version)";
+    } else if (cardNumber === 6) {
+        cardInfo = "Lagu di album Reputation: <br> 1. ...Ready For It?\n2. End Game\n3. I Did Something Bad\n4. Don't Blame Me\n5. Delicate\n6. Look What You Made Me Do\n7. So It Goes...\n8. Gorgeus\n9. Getaway Car\n10. King Of My Heart\n11. Dancing With Our Hands Tied\n12. Dress\n13. This Is Why We Can't Have Nice Things\n14. Call It What You Want\n15. New Year's Day";
+    } else if (cardNumber === 7) {
+        cardInfo = "Lagu di album Lover: <br> 1. I Forgot That You Existed\n2. Cruel Summer\n3. Lover\n4. The Man\n5. The Archer\n6. I Think He Knows\n7. Miss Americana & The Hearthbreak Prince\n8. Paper Rings\n9. Cornelia Street\n10. Death By A Thousan Cuts\n11. London Boy\n12. Soon You'll Get Better\n13. False God\n14. You Need to Calm Down\n15. Afterglow\n16. ME!\n17. It's Nice Have a Friend\n18. Daylight";
+    } else if (cardNumber === 8) {
+        cardInfo = "Lagu di album Folklore: <br> 1. the 1\n2. cardigan\n3. the last great american dinasty\n4. exile (feat. Bon Iver)\n5. my tears ricochet\n6. mirrorball\n7. seven\n8. august\n 9. this is me trying\n10. illicit affairs\n11. invisible string\n12. mad woman\n13. epiphany\n14. betty\n15. peace\n16. hoax\n17. the lakes - bonus tract";
+    } else if (cardNumber === 9) { 
+        cardInfo = "Lagu di album Evermore: <br> 1. willow\n2. champagne problems\n3. gold rush\n4. 'tis the damn season\n5. tolerate it\n6.no body, no crime\n7. happiness\n8. dorothea\n9. coney island\n10. ivy\n11. cowboy like me\n12. long stroy short\n13. marjorie\n14. closure\n15. evermore\n16. right where you left me\n17. it's time to go - bonus tract";
+    } else if (cardNumber === 10) {
+        cardInfo = "Lagu di album Midnights: <br> 1. Lavender Haze\n2. Maroon\n3. Anti-Hero\n4. Snow on The Beach\n5. You're on Your Own, Kid\n6. Midnight Rain\n7. Question...?\n8. Vigilante Shit\n9. Bejeweled\n10. Labyrinth\n11. Karma\n12. Sweet Nothing\n13. Mastermind\n14. The Great War\n15. Bigger Than thw Whole Sky\n16. Paris\n17. High Infidelity\n18. Glitch\n19. Would've, Coulde've, Should've\n20. Dear Reader";
+    }
+
+    // Tampilkan informasi di modal
+    document.getElementById('cardInfoContent').innerHTML = cardInfo.replace(/\n/g, '<br>');
+
+    // Buka modal
+    $('#cardInfoModal').modal('show');
+}
+
+const quotes = [
+    {
+        text: "I scream 'For whatever it's worth, I love you, ain't that the worst thing you ever heard?",
+        author: "Cruel Summer"
+    },
+    {
+        text: "no body, no crime but i ain't letting up until the day i die",
+        author: "no body, no crime"
+    },
+    {
+        text: "When did all our lessons start to look like weapons Pointed at my deepest hurt?",
+        author: "happiness"
+    },
+    {
+        text: "Everything you lose is a step you take",
+        author: "You're on Your Own, Kid"
+    },
+    {
+        text: "Bend when you can, snap when you have to",
+        author: "Dear Reader"
+    },
+    {
+        text: "Love is a ruthless game unless you play it good and right",
+        author: "State of Grace"
+    },
+    {
+        text: "Just because you're clean don't mean you don't miss it",
+        author: "Clean"
+    },
+    {
+        text: "Romance is not dead if you keep it just yours",
+        author: "Paris"
+    },
+    {
+        text: "When you're fifteen and somebody tells you they love you, you're gonna believe them",
+        author: "Fifteen"
+    },
+    {
+        text: "If you fails to plan you plan to fail. Strategy sets the scene for the tale",
+        author: "Mastermind"
+    },
+    {
+        text: "Never be so kind you forget to be clever. Never be so clever you forget to be kind",
+        author: "marjorie"
+    },
+    {
+        text: "If you never bleed, you're never gonna grow",
+        author: "the 1"
+    },
+    {
+        text: "Oh, darlin', don't you ever grow up",
+        author: "Never Grow Up"
+    },
+    {
+        text: "They took the crown, but it's alright",
+        author: "Call it What You Want"
+    },
+    {
+        text: "Karma's a relaxing thought",
+        author: "Karma"
+    },
+    {
+        text: "They players gonna play, and the haters gonna hate",
+        author: "Shake it Off"
+    },
+    {   
+        text: "Cause there we are again in the middle of the night. We're dancing 'round the kitchen in the refrigerator light",
+        author: "All to Well"
+    },
+    {
+        text: "it's 2 a.m., feeling like I just lost a friend. Hope you know it's not easy, easy for me",
+        author: "Breathe"
+    }
+];
+
+document.getElementById('new-quote-btn').addEventListener('click', generateQuote);
+
+function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    document.getElementById('quote-text').textContent = `"${quote.text}"`;
+    document.getElementById('author').textContent = `- ${quote.author}`;
+}
